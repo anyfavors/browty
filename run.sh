8f4b3cd9b36ee8dc5bc38d72d52ce31e41d9f185
stephanmh <stephanmh@gmail.com>
2021-06-04 19:56:22 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,18 +14,18 @@ else
   mkdir ~/.ssh && \
   ssh-keyscan -H -p ${REMOTE_SSH_PORT} ${REMOTE_SSH_SERVER} > ~/.ssh/known_hosts
 
-  cmd="npm start -- -p ${WETTY_PORT} --sshhost ${REMOTE_SSH_SERVER} --sshport ${REMOTE_SSH_PORT} --base ${BASE} --ssh-config /etc/ssh/ssh_config" 
+  cmd="npm start -- -p ${WETTY_PORT} --ssh-host ${REMOTE_SSH_SERVER} --ssh-port ${REMOTE_SSH_PORT} --base ${BASE} --ssh-config /etc/ssh/ssh_config" 
   if ! [ "x${REMOTE_SSH_USER}" == "x" ]; then
-    cmd="${cmd} --sshuser ${REMOTE_SSH_USER}"
+    cmd="${cmd} --ssh-user ${REMOTE_SSH_USER}"
   fi 
   if ! [ "x${SSH_AUTH}" == "x" ]; then
-    cmd="${cmd} --sshauth ${SSH_AUTH}"
+    cmd="${cmd} --ssh-auth ${SSH_AUTH}"
   fi
   if ! [ "x${KNOWN_HOSTS}" == "x" ]; then
-    cmd="${cmd} --knownhosts ${KNOWNHOSTS}"
+    cmd="${cmd} --known-hosts ${KNOWNHOSTS}"
   fi
   if ! [ "x${SSH_KEY}" == "x" ]; then
-    cmd="${cmd} --sshkey ${SSH_KEY}"
+    cmd="${cmd} --ssh-key ${SSH_KEY}"
   fi
   su -c "${cmd}" node
 fi
